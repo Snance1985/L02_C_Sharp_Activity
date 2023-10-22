dddf06ec68baca90d76f49b6ec5bebbaa58ca2f2
Sean Nance <sean_nance@hotmail.com>
2023-10-22 13:02:31 -0700

    created c-sharp L02-activity files

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double num1 = 2;
+double num2 = 6;
+double average = num1+num2/2;
+
+Console.WriteLine("The average of " + num1 + " and " + num2 + " is " + average);
+
+string myName = "Sean Nance";
+Console.WriteLine("My name is ".ToUpper() + myName.ToUpper() + " and I love coding.".ToUpper());
